dddbf9b78e93424b3d8e7bd27f86bd8e12e663cf
Andrei Pirogov <andrei.pirogov.huertas@gmail.com>
2022-09-26 20:34:05 +0200

    Added comments to parts of codde

--- app/src/main/java/com/example/pazaakuppgift/HandBuildingActivity.kt
+++ app/src/main/java/com/example/pazaakuppgift/HandBuildingActivity.kt
@@ -11,7 +11,7 @@ import com.google.android.material.appbar.AppBarLayout
 
 
 class HandBuildingActivity : AppCompatActivity() {
-
+    // lateinit of clickable extra cards
      lateinit var p1AvailableExtraCard1ImageButton: ImageButton
      lateinit var p1AvailableExtraCard2ImageButton: ImageButton
      lateinit var p1AvailableExtraCard3ImageButton: ImageButton
@@ -31,19 +31,24 @@ class HandBuildingActivity : AppCompatActivity() {
      lateinit var p2AvailableExtraCard8ImageButton: ImageButton
      lateinit var p2AvailableExtraCard9ImageButton: ImageButton
 
+    //     lateinit of the cards the players choose
     lateinit var p1ChosenExtraCard1ImageButton: ImageButton
     lateinit var p1ChosenExtraCard2ImageButton: ImageButton
     lateinit var p1ChosenExtraCard3ImageButton: ImageButton
     lateinit var p2ChosenExtraCard1ImageButton: ImageButton
     lateinit var p2ChosenExtraCard2ImageButton: ImageButton
     lateinit var p2ChosenExtraCard3ImageButton: ImageButton
+
+    //    lateinit of the ready buttons
     lateinit var p1HandBuildingReadyButton: Button
     lateinit var p2HandBuildingReadyButton: Button
-//    lateinit var readyToStartBlockerAppBar: AppBarLayout
 
+
+    //  boolean variable to check if player are ready to start game
     var player1Ready = false
     var player2Ready = false
 
+    //  variables that keep track of the chosen value of the extra cards
     var p1ChosenExtraCard1Value = 0
     var p1ChosenExtraCard2Value = 0
     var p1ChosenExtraCard3Value = 0
@@ -51,6 +56,7 @@ class HandBuildingActivity : AppCompatActivity() {
     var p2ChosenExtraCard2Value = 0
     var p2ChosenExtraCard3Value = 0
 
+    // placeholder player names if the player does not submit a name
     var player1Name = "Player 1"
     var player2Name = "Player 2"
 
@@ -58,25 +64,25 @@ class HandBuildingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hand_building)
-
-        p1AvailableExtraCard1ImageButton = findViewById<ImageButton>(R.id.p1AvailableExtraCard1ImageButton)
-        p1AvailableExtraCard2ImageButton = findViewById<ImageButton>(R.id.p1AvailableExtraCard2ImageButton)
-        p1AvailableExtraCard3ImageButton = findViewById<ImageButton>(R.id.p1AvailableExtraCard3ImageButton)
-        p1AvailableExtraCard4ImageButton = findViewById<ImageButton>(R.id.p1AvailableExtraCard4ImageButton)
-        p1AvailableExtraCard5ImageButton = findViewById<ImageButton>(R.id.p1AvailableExtraCard5ImageButton)
-        p1AvailableExtraCard6ImageButton = findViewById<ImageButton>(R.id.p1AvailableExtraCard6ImageButton)
-        p1AvailableExtraCard7ImageButton = findViewById<ImageButton>(R.id.p1AvailableExtraCard7ImageButton)
-        p1AvailableExtraCard8ImageButton = findViewById<ImageButton>(R.id.p1AvailableExtraCard8ImageButton)
-        p1AvailableExtraCard9ImageButton = findViewById<ImageButton>(R.id.p1AvailableExtraCard9ImageButton)
-        p2AvailableExtraCard1ImageButton = findViewById<ImageButton>(R.id.p2AvailableExtraCard1ImageButton)
-        p2AvailableExtraCard2ImageButton = findViewById<ImageButton>(R.id.p2AvailableExtraCard2ImageButton)
-        p2AvailableExtraCard3ImageButton = findViewById<ImageButton>(R.id.p2AvailableExtraCard3ImageButton)
-        p2AvailableExtraCard4ImageButton = findViewById<ImageButton>(R.id.p2AvailableExtraCard4ImageButton)
-        p2AvailableExtraCard5ImageButton = findViewById<ImageButton>(R.id.p2AvailableExtraCard5ImageButton)
-        p2AvailableExtraCard6ImageButton = findViewById<ImageButton>(R.id.p2AvailableExtraCard6ImageButton)
-        p2AvailableExtraCard7ImageButton = findViewById<ImageButton>(R.id.p2AvailableExtraCard7ImageButton)
-        p2AvailableExtraCard8ImageButton = findViewById<ImageButton>(R.id.p2AvailableExtraCard8ImageButton)
-        p2AvailableExtraCard9ImageButton = findViewById<ImageButton>(R.id.p2AvailableExtraCard9ImageButton)
+        // finding the id of elements in the layout
+        p1AvailableExtraCard1ImageButton = findViewById(R.id.p1AvailableExtraCard1ImageButton)
+        p1AvailableExtraCard2ImageButton = findViewById(R.id.p1AvailableExtraCard2ImageButton)
+        p1AvailableExtraCard3ImageButton = findViewById(R.id.p1AvailableExtraCard3ImageButton)
+        p1AvailableExtraCard4ImageButton = findViewById(R.id.p1AvailableExtraCard4ImageButton)
+        p1AvailableExtraCard5ImageButton = findViewById(R.id.p1AvailableExtraCard5ImageButton)
+        p1AvailableExtraCard6ImageButton = findViewById(R.id.p1AvailableExtraCard6ImageButton)
+        p1AvailableExtraCard7ImageButton = findViewById(R.id.p1AvailableExtraCard7ImageButton)
+        p1AvailableExtraCard8ImageButton = findViewById(R.id.p1AvailableExtraCard8ImageButton)
+        p1AvailableExtraCard9ImageButton = findViewById(R.id.p1AvailableExtraCard9ImageButton)
+        p2AvailableExtraCard1ImageButton = findViewById(R.id.p2AvailableExtraCard1ImageButton)
+        p2AvailableExtraCard2ImageButton = findViewById(R.id.p2AvailableExtraCard2ImageButton)
+        p2AvailableExtraCard3ImageButton = findViewById(R.id.p2AvailableExtraCard3ImageButton)
+        p2AvailableExtraCard4ImageButton = findViewById(R.id.p2AvailableExtraCard4ImageButton)
+        p2AvailableExtraCard5ImageButton = findViewById(R.id.p2AvailableExtraCard5ImageButton)
+        p2AvailableExtraCard6ImageButton = findViewById(R.id.p2AvailableExtraCard6ImageButton)
+        p2AvailableExtraCard7ImageButton = findViewById(R.id.p2AvailableExtraCard7ImageButton)
+        p2AvailableExtraCard8ImageButton = findViewById(R.id.p2AvailableExtraCard8ImageButton)
+        p2AvailableExtraCard9ImageButton = findViewById(R.id.p2AvailableExtraCard9ImageButton)
 
         var player1NameTextView = findViewById<TextView>(R.id.player1NameTextView)
         var player2NameTextView = findViewById<TextView>(R.id.player2NameTextVIew)
@@ -91,9 +97,9 @@ class HandBuildingActivity : AppCompatActivity() {
         p2ChosenExtraCard2ImageButton = findViewById(R.id.p2ChosenExtraCard2ImageButton)
         p2ChosenExtraCard3ImageButton = findViewById(R.id.p2ChosenExtraCard3ImageButton)
 
-//        readyToStartBlockerAppBar = findViewById(R.id.readyToStartBlockerAppBar)
 
 
+        //  clickable chosen extra cards. Disabled at the start because they have no chosen value
         p1ChosenExtraCard1ImageButton.setEnabled(false)
         p1ChosenExtraCard2ImageButton.setEnabled(false)
         p1ChosenExtraCard3ImageButton.setEnabled(false)
@@ -101,17 +107,21 @@ class HandBuildingActivity : AppCompatActivity() {
         p2ChosenExtraCard2ImageButton.setEnabled(false)
         p2ChosenExtraCard3ImageButton.setEnabled(false)
 
+        //player names fetched from the last activity if they have been submitted
         player1Name = intent.getStringExtra("player1Name").toString()
         player2Name = intent.getStringExtra("player2Name").toString()
 
+
+        // fetched player names put into the right TextView
         player1NameTextView.text = player1Name
         player2NameTextView.text = player2Name
 
-
+        //  ready buttons disabled at start until players choose 3 cards
         p1HandBuildingReadyButton.setEnabled(false)
         p2HandBuildingReadyButton.setEnabled(false)
 
-
+        //  ready buttons to start game when cards are picked, game starts when both are clicked
+        //  buttons are disabled after being clicked
         p1HandBuildingReadyButton.setOnClickListener {
             player1Ready = true
             p1HandBuildingReadyButton.setEnabled(false)
@@ -122,7 +132,10 @@ class HandBuildingActivity : AppCompatActivity() {
             p2HandBuildingReadyButton.setEnabled(false)
             checkReadyToStart()
         }
-
+        //  clicking an available card picks it as one of the chosen extra cards
+        // playerValue tells which player is clicking and chosenValue gives the card's value
+        //  button is disabled after being chosen
+        //  lastly checks if all extra cards have been chosen. If true, the ready button becomes available
         p1AvailableExtraCard1ImageButton.setOnClickListener{
             var chosenValue = 1
             var playerValue = 1
@@ -198,8 +211,10 @@ class HandBuildingActivity : AppCompatActivity() {
         }
 
 
-
-
+        //  clicking an available card picks it as one of the chosen extra cards
+        //  playerValue tells which player is clicking and chosenValue gives the card's value
+        //  button is disabled after being chosen
+        //  lastly checks if all extra cards have been chosen. If true, the ready button becomes available
         p2AvailableExtraCard1ImageButton.setOnClickListener{
             var chosenValue = 1
             var playerValue = 2
@@ -277,7 +292,12 @@ class HandBuildingActivity : AppCompatActivity() {
 
 
 
-
+        //  the chosen extra cards can be pressed to remove the chosen card in that spot
+        //  the chosen extra card is enabled again
+        //  the ready button is disabled again because the player doesnt have 3 cards
+        //  the button is disabled since it has no value
+        //  uses the given value from the picked card to set it
+        //  playerValue tells which player it belongs to
 
         p1ChosenExtraCard1ImageButton.setOnClickListener{
             var currentValue = p1ChosenExtraCard1Value
@@ -348,6 +368,9 @@ class HandBuildingActivity : AppCompatActivity() {
 
     }
 
+
+    //  reenables cards if the card is removed from the chosen hand
+    //  gets value from the removed card and also player value to know which player pressed
     fun ReenableCards(playerValue: Int, currentValue: Int){
         if (playerValue == 1){
                   when(currentValue){
@@ -419,16 +442,16 @@ class HandBuildingActivity : AppCompatActivity() {
 
 
     }
-
+    //  checks if all 3 cards have been picked for the players hand.
+    //  takes in playerValue to know which hand to check and to know which ready button to enable
+    //  also disables ready button if card has been removed from hand
     fun checkIfCardsReady(playerValue: Int){
 
         if(playerValue == 1) {
             if (p1ChosenExtraCard1Value != 0 && p1ChosenExtraCard2Value != 0 && p1ChosenExtraCard3Value != 0) {
                 p1HandBuildingReadyButton.setEnabled(true)
-//                readyToStartBlockerAppBar.setVisibility(View.VISIBLE)
             } else {
                 p1HandBuildingReadyButton.setEnabled(false)
-//                readyToStartBlockerAppBar.setVisibility(View.INVISIBLE)
             }
         }else if(playerValue == 2) {
             if (p2ChosenExtraCard1Value != 0 && p2ChosenExtraCard2Value != 0 && p2ChosenExtraCard3Value != 0) {
@@ -438,7 +461,9 @@ class HandBuildingActivity : AppCompatActivity() {
             }
         }
     }
-
+    //  checks if both players have pressed the ready button
+    //  if both players are ready then it initiates the next activity
+    //  sends the chosen player names and the chosen extra cards to the next activity
     fun checkReadyToStart(){
         if(player1Ready == true && player2Ready == true){
             val intent = Intent(this, MainActivity::class.java)
@@ -450,7 +475,9 @@ class HandBuildingActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
+    //  gives the cards in the hand value based on the ones chosen from the board
+    //  takes in cardValue to know the chosen value for the card
+    // takes in playerValue to know which player chose it
     fun chooseCardAsExtra(cardValue: Int,playerValue: Int) {
         if(playerValue== 1) {
             if (p1ChosenExtraCard1Value == 0) {
@@ -477,6 +504,7 @@ class HandBuildingActivity : AppCompatActivity() {
         }
     }
 
+    //  assigns an image to the cards in the players' hands based on the ones picked from the board
     fun setCardImage(){
         when (p1ChosenExtraCard1Value){
 
